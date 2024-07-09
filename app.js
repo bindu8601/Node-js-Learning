605b18d92d8def574b69d305934e1b2ce2160f6c
Bindu Eppalapalle <127506733+bindu8601@users.noreply.github.com>
2024-07-09 20:43:00 +0530

    prasing req body

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const http=require('http')
+const fs= require('fs')
 //first method
 // function rqListener(req,res){
 // console.log(req?.url,req?.headers);
 // }
 const server =http.createServer((req,res)=>{
-    console.log(req.url)
+    const url=req.url
+    const method=req.method
+    //conditionally sending response accordingly request
+    if(url==='/'){
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submitt</button></form></body>')
+        res.write('</html>')
+        return res.end();
+    }
+    //redirecting requests
+    if(url==='/message' && method==='POST'){
+        const body=[]
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk)
+        })
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString()
+            console.log(parsedBody)
+            const message=parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt',message);
+
+        })
+        res.statusCode=302;
+        res.setHeader('Location','/')
+        return res.end()
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html>')
     res.write('<head><title>My First Page</title><head>')
