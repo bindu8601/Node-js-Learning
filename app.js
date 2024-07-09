bf0f0b6eca03af8474485e933803731a639ffd03
Bindu Eppalapalle <127506733+bindu8601@users.noreply.github.com>
2024-07-09 11:01:57 +0530

    handling responses in node js

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const http=require('http')
 //first method
-function rqListener(req,res){
-console.log(req);
-}
-const server =http.createServer(rqListener);
+// function rqListener(req,res){
+// console.log(req?.url,req?.headers);
+// }
+const server =http.createServer((req,res)=>{
+    console.log(req.url)
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>My First Page</title><head>')
+    res.write('<body><h1>Hello</h1></body>')
+    res.write('</html>')
+    res.end()
+});
 
 //second method
 // http.createServer(function(req,res){
