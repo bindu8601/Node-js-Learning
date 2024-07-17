2fcb3b320d678ca8e3c21ff52aef798985ec76dc
Bindu Eppalapalle <127506733+bindu8601@users.noreply.github.com>
2024-07-17 11:32:49 +0530

    using body parser

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const http=require('http')
 
 const express= require('express')
 
+const bodyParser=require('body-parser')
+
 const app= express()
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submitt">Submit</button></form>')
+})
+
 app.use('/product',(req,res,next)=>{
-    console.log('in the another middleware');
-    res.send('<h1>This is a product</h1>')
+    console.log(req.body);
+    res.redirect('/')
 })
 
 app.use('/',(req,res,next)=>{
