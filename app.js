0e9deb139e169b6f4dfbe6ca913d8a5033afbc90
Bindu Eppalapalle <127506733+bindu8601@users.noreply.github.com>
2024-07-17 11:11:45 +0530

    understanding middlewares

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const http=require('http')
 const express= require('express')
 
 const app= express()
+app.use((req,res,next)=>{
+    console.log("In the middleware");
+    next() // allows the request to continue to the next middleware in line 
+})
+app.use((req,res,next)=>{
+    console.log('in the another middleware');
+    res.send('<h1>Hello From Express</h1>')
+})
 
-const server =http.createServer(app);
-
-
-server.listen(3000)
+// const server =http.createServer(app);
+// server.listen(3000)
+app.listen(3000)
